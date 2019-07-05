fee045e1ac37b906f969d22d9c3949c2e9ed978c
Kelsey Low <kklowlow@gmail.com>
2019-07-05 04:22:14 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,11 @@
 module ArtistsHelper
+  def display_artist(song)
+    if song.artist
+      link_to song.artist_name, artist_path(song.artist)
+      # link to artist's show page
+    else
+      link_to "Add Artist", edit_song_path(song)
+      # link to song's edit page with link text "Add Artist"
+    end
+  end
 end
